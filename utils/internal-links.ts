616b0aa5eccdd68c54f0c81a28116572cd19b8f2
Alex Schachne <alex@f.inc>
2023-08-13 08:31:22 +0300

    Clean up Restful API section, add quickstart cards

--- utils/internal-links.ts
+++ utils/internal-links.ts
@@ -2,6 +2,7 @@ export const internalLinks = {
   typeScriptSdk: "/sdk/typescript-sdk",
   typeScriptSdk_images: "/sdk/generating-images",
   typeScriptSdk_models: "/sdk/training-models",
+  typeScriptSdk_music: "/sdk/generating-music",
   typeScriptSdk_types: "/sdk/types",
   typeScriptSdk_types_inference: "/sdk/types#inference",
   typeScriptSdk_types_image: "/sdk/types#image",
@@ -12,5 +13,7 @@ export const internalLinks = {
   builds_remixMyFace: "/builds/remix-my-face",
   builds_drawIt: "/builds/draw-it",
   builds_wallpapersFyi: "https://github.com/leap-ai/wallpapers-ai",
+  builds_avatars: "https://github.com/leap-ai/leap-ai-avatars",
+  builds_coverImages: "https://github.com/leap-ai/leap-ai-cover-images",
   restful_api: "/api-docs/images",
 };
